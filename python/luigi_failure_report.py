36d67971dee79a4fc605e08106ad5f75a64ce6c4
Ben Copeland <Ben.Copeland@milliman.com>
2021-04-19 12:02:33 -0400

    Lowercase the jenkins username
    This may help with auth'ing

--- python/luigi_failure_report.py
+++ python/luigi_failure_report.py
@@ -20,7 +20,7 @@ import pandas as pd
 import psycopg2
 
 LOGGER = logging.getLogger(__name__)
-DEFAULT_USER = "Indy_Jenkins_Luigi_DB"
+DEFAULT_USER = "indy_jenkins_luigi_db"
 
 
 def query_task_history(

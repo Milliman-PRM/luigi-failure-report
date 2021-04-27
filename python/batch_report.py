1fdd0adea03dc76404cfc928e263393ce3634005
Sheamus K. Parkes <shea.parkes@milliman.com>
2021-04-27 13:20:35 -0400

    Add myself as well.
    Do this so I can be sure to experience the fun.

--- python/batch_report.py
+++ python/batch_report.py
@@ -17,7 +17,7 @@ LOGGER = logging.getLogger(__name__)
 
 BATCH_PARAMS = [
     (["0273", "1111"], ["ben.copeland@milliman.com", "daniel.buis@milliman.com", "shea.parkes@milliman.com", "holly.moore@milliman.com"]),
-    (["1111", "1112", "1113"], ["kate.fitch@milliman.com", "jonah.broulette@milliman.com", "michele.berrios@milliman.com", "pierre.cornell@milliman.com"]),
+    (["1111", "1112", "1113"], ["kate.fitch@milliman.com", "michele.berrios@milliman.com", "pierre.cornell@milliman.com", "jonah.broulette@milliman.com", "shea.parkes@milliman.com"]),
     # (["0273WSP"], ["ben.copeland@milliman.com", "daniel.buis@milliman.com"]),
     # (['1111'], 'pierre.cornell@milliman.com~umang.gupta@milliman.com')
 ]
